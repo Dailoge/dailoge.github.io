b55ff22b678d46a3ef818e47f831e911ab64b447
Dailoge <1291804627@qq.com>
2024-05-29 01:13:27 +0800

    feat: 变更小程序云地址

--- src/services/request.ts
+++ src/services/request.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // 腾讯云 serverless，
 const request = axios.create({
-  baseURL: 'https://service-fxf0odwp-1252010818.sh.apigw.tencentcs.com',
+  // baseURL: 'https://service-fxf0odwp-1252010818.sh.apigw.tencentcs.com',
+  baseURL: 'https://service-q4mj86xx-1252010818.sh.tencentapigw.com',
   timeout: 5000,
 });
 
